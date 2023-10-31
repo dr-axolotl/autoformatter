28066aa702abed7068fdcd75efba337d3ba80cbc
draxolotl <draxolotl@cingulate.dev>
2023-10-31 01:03:48 +0000

    latest sed config

--- server.js
+++ server.js
@@ -2,14 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const basicAuth = require('express-basic-auth');
 const child_process = require('child_process');
+const https = require('https');
 const fs = require('fs');
 const path = require('path');
 
+// Middleware
 const app = express();
-
 app.use(bodyParser.json());
 app.use(basicAuth({ users: { [process.env.USERNAME]: process.env.PASSWORD }, challenge: true }));
 
+// Routes
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -25,7 +27,7 @@ app.post('/convert', (req, res) => {
         let sedCommands = data.split('\n');
         sedCommands.forEach(command => {
             try {
-                input = child_process.execSync(`echo "${input.replace(/"/g, '\\"')}" | sed '${command}'`).toString();
+                input = child_process.execSync(`echo "${input.replace(/"/g, '\\"')}" | sed "${command}"`).toString();
             } catch (err) {
                 console.error(err);
                 res.status(500).json({ error: 'Error executing sed command' });
@@ -36,5 +38,14 @@ app.post('/convert', (req, res) => {
     });
 });
 
-app.listen(3000, '0.0.0.0', () => console.log('Server running on port 3000'));
+// HTTPS options
+const options =  {
+    key: fs.readFileSync('/etc/letsencrypt/live/cingulate.dev/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/cingulate.dev/cert.pem')
+};
+
+// HTTPS Server
+https.createServer(options, app).listen(3001, '0.0.0.0', () => {
+    console.log('Server running on port 3001');
+});
 
